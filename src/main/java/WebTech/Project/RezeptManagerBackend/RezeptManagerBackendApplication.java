8377a4dd82ecaf00adf964356bc015c54bc2172e
Vasili <s0569216@htw-berlin.de>
2023-04-18 13:20:46 +0200

    1. Milestone

---
+++ src/main/java/WebTech/Project/RezeptManagerBackend/RezeptManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package WebTech.Project.RezeptManagerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezeptManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WebTech/Project/RezeptManagerBackend/RezeptManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package WebTech.Project.RezeptManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezeptManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
