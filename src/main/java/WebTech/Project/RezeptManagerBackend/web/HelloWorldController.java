600c8944d83dfec15c8d0c953df78883f71b561a
Vasili <s0569216@htw-berlin.de>
2023-05-15 17:43:58 +0200

    add tests.yml for Ci

--- src/main/java/WebTech/Project/RezeptManagerBackend/web/HelloWorldController.java
+++ src/main/java/WebTech/Project/RezeptManagerBackend/web/HelloWorldController.java
@@ -2,9 +2,10 @@ package WebTech.Project.RezeptManagerBackend.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HelloWorldController {
     @GetMapping(path = "/")
     public ModelAndView showHelloWorld() {
